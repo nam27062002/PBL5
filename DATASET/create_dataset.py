5f2d7d81221d8f0eae3f12215d6c75854affd216
nam tran trong <nam.tran-trong@ubisoft.com>
2024-02-22 18:05:26 +0700

    update

---
+++ DATASET/create_dataset.py
@@ -0,0 +1,34 @@
+import os
+import cv2
+DATA_DIR = './DATASET/data0'
+if not os.path.exists(DATA_DIR):
+    os.makedirs(DATA_DIR)
+dataset_size = 100
+words = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+cap = cv2.VideoCapture(2)
+for j in words:
+    if not os.path.exists(os.path.join(DATA_DIR, str(j))):
+        os.makedirs(os.path.join(DATA_DIR, str(j)))
+
+    print('Collecting data for class {}'.format(j))
+
+    done = False
+    while True:
+        ret, frame = cap.read()
+        cv2.putText(frame, 'Ready? Press "Q" ! :)', (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3,
+                    cv2.LINE_AA)
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(25) == ord('q'):
+            break
+
+    counter = 0
+    while counter < dataset_size:
+        ret, frame = cap.read()
+        cv2.imshow('frame', frame)
+        cv2.waitKey(25)
+        cv2.imwrite(os.path.join(DATA_DIR, str(j), '{}.jpg'.format(counter)), frame)
+
+        counter += 1
+
+cap.release()
+cv2.destroyAllWindows()
