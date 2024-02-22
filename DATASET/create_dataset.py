d95bf803948ef8854e7ebb0ef524545b6afcdf1f
Nam Tran Trong <nam.tran-trong@ubisoft.com>
2024-02-22 23:56:22 +0700

    update

--- DATASET/create_dataset.py
+++ DATASET/create_dataset.py
@@ -1,18 +1,24 @@
 import os
 import cv2
-DATA_DIR = './DATASET/data0'
-if not os.path.exists(DATA_DIR):
-    os.makedirs(DATA_DIR)
-dataset_size = 100
-words = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-cap = cv2.VideoCapture(2)
-for j in words:
-    if not os.path.exists(os.path.join(DATA_DIR, str(j))):
-        os.makedirs(os.path.join(DATA_DIR, str(j)))
 
-    print('Collecting data for class {}'.format(j))
 
-    done = False
+def create_directory_if_not_exists(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+
+def main():
+    DATA_DIR = './data0'
+    create_directory_if_not_exists(DATA_DIR)
+
+    dataset_size = 100
+    words = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+             'V', 'W', 'X', 'Y', 'Z']
+    cap = cv2.VideoCapture(0)
+    index = 3
+    target_directory = os.path.join(DATA_DIR, str(words[index]))
+    create_directory_if_not_exists(target_directory)
+
     while True:
         ret, frame = cap.read()
         cv2.putText(frame, 'Ready? Press "Q" ! :)', (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3,
@@ -26,9 +32,12 @@ for j in words:
         ret, frame = cap.read()
         cv2.imshow('frame', frame)
         cv2.waitKey(25)
-        cv2.imwrite(os.path.join(DATA_DIR, str(j), '{}.jpg'.format(counter)), frame)
-
+        cv2.imwrite(os.path.join(target_directory, '{}.jpg'.format(counter)), frame)
         counter += 1
 
-cap.release()
-cv2.destroyAllWindows()
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
