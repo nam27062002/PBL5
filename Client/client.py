0727f057f5088388f10e34e3354f8df8f0808b32
nam tran trong <nam.tran-trong@ubisoft.com>
2024-03-01 18:51:54 +0700

    update

--- Client/client.py
+++ Client/client.py
@@ -1,23 +1,8 @@
 import requests
 from PIL import Image
 import io
-import os
-import time
-url = 'http://127.0.0.1:8000/predict/'
-labels = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'del', 'nothing', 'space']
-def find_jpg_files():
-    folder_path = "../Training/DATASET/asl_alphabet_train"
-    jpg_files = []
-    for root, dirs, files in os.walk(folder_path):
-        for file in files:
-            if file.lower().endswith(".jpg"):
-                jpg_files.append(os.path.join(root, file))
-    return jpg_files
-def get_parent_folder(file_path):
-    parent_folder_path = os.path.dirname(file_path)
-    parent_folder_name = os.path.basename(parent_folder_path)
-    return parent_folder_name
 
+url = 'http://127.0.0.1:8000/predict/'
 def preprocess_image(image_path):
     image = Image.open(image_path)
     image_resized = image.resize((64, 64))
@@ -26,20 +11,6 @@ def preprocess_image(image_path):
     image_io.seek(0)
     return image_io
 
-total_response_time = 0
-num_requests = 0
-
-all_files = find_jpg_files()
-
-count = 0
-for file in all_files:
-    processed_image = preprocess_image(file)
-    start_time = time.time()
-    response = requests.post(url, files={'file': processed_image})
-    end_time = time.time()
-    response_time = end_time - start_time
-    total_response_time += response_time
-    num_requests += 1
-    print(f"Response time for {file}: {response_time} seconds")
-    print(response)
-    break
+processed_image = preprocess_image('../Training/DATASET/asl_alphabet_test/D_test.jpg')
+response = requests.post(url, files={'file': processed_image})
+print(response.json())
--- Server/generate_file.py
+++ Server/generate_file.py
@@ -1,7 +1,52 @@
-import tensorflow as tf
-model = tf.keras.models.load_model('asl_vgg16_best_weights.h5')
+import requests
+from PIL import Image
+import io
+import os
+import time
 
-model_json = model.to_json()
-with open('model.json', 'w') as json_file:
-    json_file.write(model_json)
-model.save_weights('model_weights.h5')
+url = 'http://127.0.0.1:8000/predict/'
+labels = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'del', 'nothing', 'space']
+
+def find_jpg_files():
+    folder_path = "../Training/DATASET/asl_alphabet_train"
+    jpg_files = []
+    for root, dirs, files in os.walk(folder_path):
+        for file in files:
+            if file.lower().endswith(".jpg"):
+                jpg_files.append(os.path.join(root, file))
+    return jpg_files
+
+def get_parent_folder(file_path):
+    parent_folder_path = os.path.dirname(file_path)
+    parent_folder_name = os.path.basename(parent_folder_path)
+    return parent_folder_name
+
+def preprocess_image(image_path):
+    image = Image.open(image_path)
+    image_resized = image.resize((64, 64))
+    image_io = io.BytesIO()
+    image_resized.save(image_io, format='JPEG', quality=70)
+    image_io.seek(0)
+    return image_io
+
+total_response_time = 0
+num_requests = 0
+
+all_files = find_jpg_files()
+
+count = 0
+all_time = 0
+N = 100
+for file in all_files:
+    processed_image = preprocess_image(file)
+    start_time = time.time()
+    response = requests.post(url, files={'file': processed_image})
+    end_time = time.time()
+    response_time = end_time - start_time
+    total_response_time += response_time
+    num_requests += 1
+    all_time += response_time
+    count += 1
+    if count == N:
+        break
+print(all_time / N)
